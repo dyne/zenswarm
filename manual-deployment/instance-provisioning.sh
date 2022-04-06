a88994a1837fc0f7cae5dc4aeeddfd57dc512550
andrea-tabbles <andrea@tabbles.net>
2022-04-06 11:58:07 +0200

    updated instance-provisioning and saved_file.sh
    we prepared the private keys generation and announce, to be executed via non exposed JS code

--- manual-deployment/instance-provisioning.sh
+++ manual-deployment/instance-provisioning.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-set -exuo pipefail
+set -euo pipefail
 
 # TWEAK SETTINGS
-NODES=4
+NODES=42
 REPO=https://github.com/dyne/restroom-vmlet-template
 CONTRACTS=saved_files.sh
 
@@ -10,8 +10,7 @@ CONTRACTS=saved_files.sh
 BASEPATH=$(pwd)
 # HOST=$(hostname -I)
 # changed to (hostname) cause the current staging uses a shared ip
-HOST=$(hostname)
-PORTS=()
+HOST=$(hostname -I)
 
 # DEPENDENCIES
 command -v python || (sudo apt install -y python)
@@ -31,7 +30,7 @@ link_build() {
     yarn link
     cd ..
   fi
-} 
+}
 
 link_build bitauth/libauth
 link_build cryptocoinjs/secp256k1-node
@@ -39,35 +38,10 @@ link_build protobufjs/protobuf.js
 link_build mapbox/node-sqlite3
 link_build PeculiarVentures/pkcs11js
 
-# PORTS
-get_available_ports() {
-  local PORTS=()
-  read min MAX < /proc/sys/net/ipv4/ip_local_port_range
-  PORTS_NUM=$NODES*2
-
-  for _p in $(seq $min $MAX); do
-    ss -lpn | grep -q ":$_p"
-    if [[ $? -eq 0 ]]; then
-      continue
-    fi
-    PORTS+=($_p)
-    PORTS_NUM=$PORTS_NUM-1
-    if [[ $PORTS_NUM -lt 1 ]]; then
-      break;
-    fi
-  done
- 
-  echo ${PORTS[@]}
-}
-
-PORTS=( $(get_available_ports) )
-
 parallel_job() {
   local i=$1
   NODE=restroom-mw-$i
   WD=$BASEPATH/vmlets/$NODE
-  HTTP=${PORTS[$i*2-2]}
-  HTTPS=${PORTS[$i*2-1]}
 
   if ! test -d $WD; then
     npx degit $REPO $WD
@@ -80,23 +54,21 @@ parallel_job() {
     yarn add zeromq@5 --zmq-external
     yarn
 
-    if ! test -d contracts; then
-      mkdir contracts
-    fi
-    bash $BASEPATH/$CONTRACTS
-    echo "{\"identity\":{\"uid\":\"random\",\"ip\":\"$HOST\",\"baseUrl\":\"http://$HOST\",\"port_http\":\"$HTTP\",\"port_https\":\"$HTTPS\",\"public_key\":\"BGiQeHz55rNc/k/iy7wLzR1jNcq/MOy8IyS6NBZ0kY3Z4sExlyFXcILcdmWDJZp8FyrILOC6eukLkRNt7Q5tzWU=\",\"version\":\"2\",\"announceAPI\":\"/api/consensusroom-announce\",\"get-6-timestampsAPI\":\"/api/consensusroom-get-6-timestamps\",\"timestampAPI\":\"/api/consensusroom-get-timestamp\",\"tracker\":\"https://apiroom.net/\"}}" > ./contracts/identity.keys
-    echo ✔ Imported identity.keys
-    echo "CUSTOM_404_MESSAGE=nothing to see here
-    HTTP_PORT=$HTTP
-    HTTPS_PORT=$HTTPS
-    FILES_DIR=$(pwd)
-    CHAIN_EXT=chain
-    # OPENAPI=true
-    YML_EXT=yml"> .env
-    cd ..
   fi
-
   cd $WD
+  if ! test -d contracts; then
+    mkdir contracts
+  fi
+  bash $BASEPATH/$CONTRACTS
+  echo "CUSTOM_404_MESSAGE=nothing to see here
+  ZENCODE_DIR="$(pwd)/contracts"
+  FILES_DIR=$(pwd)
+  CHAIN_EXT=chain
+  # OPENAPI=true
+  HOST=$HOST
+  PRIVATE_ZENCODE_DIR="$BASEPATH/private_zencode_dir"
+  YML_EXT=yml"> .env
+
   pm2 delete $NODE || true
   pm2 start yarn --update-env --name $NODE --time -- start
 }
@@ -104,4 +76,3 @@ parallel_job() {
 for i in $(seq $NODES); do
   parallel_job $i &
 done
-
