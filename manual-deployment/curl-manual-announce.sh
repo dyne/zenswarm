812ba99366728779a54cc90201ee44128969025a
andrea-tabbles <andrea@tabbles.net>
2022-04-08 18:24:26 +0200

    cleaned up old stuff

--- manual-deployment/curl-manual-announce.sh
+++
@@ -1,4 +0,0 @@
-data=$(cat ./data.json)
-echo "$data"
-
-curl "https://apiroom.net/api/dyneorg/consensusroom-server-add-identity" -H "accept: application/json" -H "Content-Type: application/json" -d "$data"
