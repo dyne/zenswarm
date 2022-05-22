b558cb4f7e8e4166dbd12c06dcfe9f2575fd95a0
Puria Nafisi Azizi <puria@dyne.org>
2022-05-22 19:50:32 +0200

    Add automagic deployment for production server

---
+++ manual-deployment/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  deploy: {
+    production: {
+      path: "/",
+      host: ["vmlets"],
+      ref : "origin/main",
+      repo: "https://github.com/dyne/zenswarm.git",
+      "post-setup": "ls -la",
+      "post-deploy": "cd /root/zenswarm && bash manual-deployment/instance-provisioning.sh && pm2 startOrGracefulReload ecosystem.config.js --env production"
+    }
+  }
+}
