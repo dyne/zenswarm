ce5a81158bef32b4a3430784650a68c21f7e6dab
Alberto Lerda <albertolerda97@gmail.com>
2022-06-21 15:33:08 +0200

    feat: provision with ansible of oracles and tls cert

--- ansible/run_ansible_example.sh
+++ ansible/run_ansible_example.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-ansible-playbook instance-provisioning.yml -i hosts.toml -e '{ "announce_url": "https://apiroom.net/api/dyneorg/zenswarm-server-add-identity-dev", "deannounce_url": "https://apiroom.net/api/dyneorg/zenswarm-issuer-remove-identity-dev" }'
+# ansible-playbook instance-provisioning.yml -i hosts.toml -e '{ "announce_url": "https://apiroom.net/api/dyneorg/zenswarm-server-add-identity-dev", "deannounce_url": "https://apiroom.net/api/dyneorg/zenswarm-issuer-remove-identity-dev" }'
+ansible-playbook start-instances.yml -i hosts.toml -e '{ "announce_url": "https://apiroom.net/api/dyneorg/zenswarm-server-add-identity-dev-chain.chain", "deannounce_url": "https://apiroom.net/api/dyneorg/zenswarm-issuer-remove-identity-dev-DID" }'
